a081fecb542a77c5b709f72ef47cf6d206731916
Suryansh Tiwari <tiwarisuryansh6222@gmail.com>
2026-02-10 02:10:40 +0530

    Fix frontend routing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import ViewPapers from "./pages/ViewPapers";
 import Upload from "./pages/Upload";
-import Admin from "./pages/Admin";
+import ViewPapers from "./pages/ViewPapers";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/view" element={<ViewPapers />} />
         <Route path="/upload" element={<Upload />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/view" element={<ViewPapers />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>PYQ Portal</h1>;
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>PYQ Portal</h1>
+
+      <Link to="/upload">
+        <button>Upload Paper</button>
+      </Link>
+
+      <Link to="/view" style={{ marginLeft: 10 }}>
+        <button>View Papers</button>
+      </Link>
+    </div>
+  );
 }
